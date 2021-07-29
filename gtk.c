1b60b78368b2098afedcf7ee75e2e12bdeefe494
DataResearch <developGames@gmx.de>
2021-07-29 11:23:39 +0200

    added another button for subtraction

--- gtk.c
+++ gtk.c
@@ -17,6 +17,16 @@ void do_calculate(GtkWidget *calculate, gpointer data) {
     gtk_label_set_text(GTK_LABEL(result), buffer);
 }
 
+void do_calculate_2(GtkWidget *calculate, gpointer data) {
+    int num1 = atoi((char *)gtk_entry_get_text(GTK_ENTRY(number1)));
+    int num2 = atoi((char *)gtk_entry_get_text(GTK_ENTRY(number2)));
+
+    char buffer[32];
+    snprintf(buffer, sizeof(buffer), "result: %d", num1 - num2);
+
+    gtk_label_set_text(GTK_LABEL(result), buffer);
+}
+
 void do_quersumme(GtkWidget *qs, gpointer data) {
     int num1 = atoi((char*)gtk_entry_get_text(GTK_ENTRY(number1)));
     int num2 = atoi((char*)gtk_entry_get_text(GTK_ENTRY(number2)));
@@ -49,13 +59,17 @@ int main(int argc, char **argv) {
     g_signal_connect(calculate, "clicked", G_CALLBACK(do_calculate), NULL);
     gtk_grid_attach(GTK_GRID(grid), calculate, 2, 0, 1, 1);
 
+    calculate = gtk_button_new_with_label("calculatos numero 2");
+    g_signal_connect(calculate, "clicked", G_CALLBACK(do_calculate_2), NULL);
+    gtk_grid_attach(GTK_GRID(grid), calculate, 3, 0, 1, 1);
+
     qs = gtk_button_new_with_label("quersumme_der_summe");
     g_signal_connect(qs, "clicked",
     G_CALLBACK(do_quersumme),NULL);
-    gtk_grid_attach(GTK_GRID(grid), qs, 3, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), qs, 4, 0, 1, 1);
 
     result = gtk_label_new("result:");
-    gtk_grid_attach(GTK_GRID(grid), result, 4, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), result, 5, 0, 1, 1);
 
     gtk_widget_show_all(window);
     gtk_main();
